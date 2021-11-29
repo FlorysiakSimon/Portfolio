78b83716d8c3318aa817a920f3d095ed5436e6ea
simon <simon.florysiak@gmail.com>
2021-11-29 12:09:31 +0100

    3d test

--- src/pages/WorkPage/WorkPage.jsx
+++ src/pages/WorkPage/WorkPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './WorkPage.scss'
 import Project from '../../components/Project/Project'
+import Model from '../../components/Model/Model'
 
 export default function WorkPage() {
     return (
+        
         <div className="work">
+            <div>
+            {/* <Model /> */}
+            </div>
             <Project />
         </div>
     )
