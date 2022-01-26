df9d689ddaeddeb4da421d94b1d865f3d2767026
simon <simon.florysiak@gmail.com>
2022-01-26 18:32:02 +0100

    update data

---
+++ src/data/data.js
@@ -0,0 +1,58 @@
+export const projets = [
+	{
+		title: 'Les petits plats',
+		link:'https://github.com/FlorysiakSimon/Les-petits-plats',
+		description: 'Algorithme de recherche en JavaScript pour un site de cuisine',
+		tags: ["JavaScript","JSBench","SASS"],
+		picture: 'https://camo.githubusercontent.com/60111f214e337ed493ebbf82cd658519f74c19988d21611c1b11f2eabfee9469/68747470733a2f2f692e6779617a6f2e636f6d2f61383637333962636634333465376432396163386534363139643461613031312e706e67'
+	},
+	{
+		title: 'FishEye',
+		link:'https://github.com/FlorysiakSimon/FishEye',
+		description: 'Site accessible pour une plateforme de photographes',
+		tags: ["JavaScript","CSS","Accessibility","ARIA"],
+		picture: 'https://camo.githubusercontent.com/cae5d29b38015c1c49b99155f2385fafa0158ba3034d7b8031517d006e71064c/68747470733a2f2f692e6779617a6f2e636f6d2f63646533343362653766343661646634373238653530666239336638366163642e706e67'
+	},
+	{
+		title: 'Kasa',
+		link:'https://github.com/FlorysiakSimon/Kasa',
+		description: 'Application Web avec React et React Router',
+		tags: ["React","React-router","JavaScript","SASS","prop-types"],
+		picture: 'https://camo.githubusercontent.com/d832d832f6fb576b5924043058f411efaf67ae96986f38100771c9a2ccfd618c/68747470733a2f2f692e6779617a6f2e636f6d2f31623837343939666531386634333532353433623632666330343761663163392e6a7067'
+	},
+	{
+		title: 'SportSee',
+		link:'https://github.com/FlorysiakSimon/SportSee',
+		description: 'Tableau de bord analytics avec React',
+		tags: ["React","React-router","JavaScript","Recharts","Axios","prop-types"],
+		picture: 'https://camo.githubusercontent.com/3418733bb5591556a58c2ce65e16b1c5fbfed655db556e650d3e91ded6705462/68747470733a2f2f692e6779617a6f2e636f6d2f32386338356430656432393837656365626631363237353835333239383734392e706e67'
+	},
+	{
+		title: 'ArgentBank',
+		link:'https://github.com/FlorysiakSimon/ArgentBank',
+		description: 'Utiliser une API pour un compte bancaire avec React',
+		tags: ["React","React-router","Redux","Persist","JavaScript","SASS","Axios","Swagger","JWToken"],
+		picture: 'https://camo.githubusercontent.com/dfbd00926ccf0f47152ea03a59ac0cbc2ce6e9ea3cfda40b387b8a61076f3223/68747470733a2f2f692e6779617a6f2e636f6d2f64623062383966346132653438316334366334636163343963643330313037382e706e67'
+	},
+	{
+		title: 'HRnet',
+		link:'https://github.com/FlorysiakSimon/HRnet',
+		description: 'Passer une librairie jQuery vers React',
+		tags: ["React","React-router","jQuery","JavaScript","SASS","npm package"],
+		picture: 'https://camo.githubusercontent.com/440504efd2d02a8e868aee404604fbb2227b0df7640bc88ef02c50f681c96410/68747470733a2f2f692e6779617a6f2e636f6d2f65646230373463316663303962373531656162356230643436333262323066382e706e67'
+	},
+	{
+		title: 'r-modal-sf',
+		link:'https://www.npmjs.com/package/r-modal-sf',
+		description: "Création d'un composant React modal personnalisable sur npm",
+		tags: ["React","npm","JavaScript","Storybook","prop-types"],
+		picture: 'https://i.gyazo.com/af60767e0af9950f241cb9b8a56c9a47.png'
+	},
+	{
+		title: 'rest-countries-api',
+		link:'https://github.com/FlorysiakSimon/rest-countries-api',
+		description:'REST Countries API avec un color theme switcher',
+		tags:["React","react-router","axios","api","SASS"],
+		picture:"https://raw.githubusercontent.com/FlorysiakSimon/rest-countries-api/main/design/desktop-preview.jpg"
+	},
+];
